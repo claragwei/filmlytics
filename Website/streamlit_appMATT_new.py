91ac3cbcc600f90b5bdd450350ffc0fa1b78e8b2
Clara Wei <159494840+claragwei@users.noreply.github.com>
2025-12-06 11:08:56 -0800

    streamlit cloud setup

--- Website/streamlit_appMATT_new.py
+++ Website/streamlit_appMATT_new.py
@@ -14,7 +14,8 @@ import pickle # For loading stacking model
 # CONFIGURATION
 
 # MongoDB Connection
-MONGODB_URI = "mongodb+srv://cinemaniacs:filmlytics@filmlytics.1emhcue.mongodb.net/?appName=filmlytics"
+# MONGODB_URI = "mongodb+srv://cinemaniacs:filmlytics@filmlytics.1emhcue.mongodb.net/?appName=filmlytics"
+MONGODB_URI = st.secrets["MONGODB_URI"]
 
 # Page Configuration
 st.set_page_config(
